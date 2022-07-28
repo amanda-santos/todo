5ed4a848063056178dd96142c967c0fe65db7053
amanda-santos <amandasf0200@gmail.com>
2022-07-28 20:02:35 -0300

    feat: 💄 add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,14 @@
-export const App = () => {
-  return <div>Todo</div>;
+import { ReactElement } from "react";
+import { ThemeProvider } from "styled-components";
+
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
+
+export const App = (): ReactElement => {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      Todo
+      <GlobalStyle />
+    </ThemeProvider>
+  );
 };
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,19 @@
+export const defaultTheme = {
+  "primary-base": "#8284FA",
+  "primary-dark": "#5E60CE",
+
+  "secondary-base": "#4EA8DE",
+  "secondary-dark": "#1E6F9F",
+
+  "danger-base": "#E25858",
+
+  "gray-700": "#0D0D0D",
+  "gray-600": "#1A1A1A",
+  "gray-500": "#262626",
+  "gray-400": "#333333",
+  "gray-300": "#808080",
+  "gray-200": "#D9D9D9",
+  "gray-100": "#F2F2F2",
+};
+
+export type ThemeType = typeof defaultTheme;
